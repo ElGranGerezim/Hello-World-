990abd678d79c888cd2de53707288f4ea931726b
ElGranGerezim <nazanal@live.com>
2022-09-17 10:09:15 -0600

    Enlarged World

--- Hello World/Hello World.cpp
+++ Hello World/Hello World.cpp
@@ -10,5 +10,13 @@ int main()
     cout << "H  H  E     L     L     O  O\n";
     cout << "HHHH  EEE   L     L     O  O\n";
     cout << "H  H  E     L     L     O  O\n";
-    cout << "H  H  EEEE  LLLL  LLLL  OOOO\n";
+    cout << "H  H  EEEE  LLLL  LLLL  OOOO\n" << endl;
+                                                
+    cout << "W   W OOOO  RRRR  L     DDD \n";
+    cout << "W   W O  O  R  R  L     D DD\n";
+    cout << "W W W O  O  RRRR  L     D  D\n";
+    cout << "WW WW O  O  R R   L     D DD\n";
+    cout << "W   W OOOO  R  R  LLLL  DDD \n";
+
+    return 1;
 }
